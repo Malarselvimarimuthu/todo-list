7a1b51cdaab7ffe9640ec4c64393d392cdd351cd
Malarselvi <156997996+Malarselvimarimuthu@users.noreply.github.com>
2025-05-06 12:11:55 +0530

    Updated Admin CRUD Page

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'; 
 const Todo = () => {
+
+  const[todo,setTodo] = useState('');
+  const[db,setDb] = useState([]);
+ 
+  
+  function posts(){
+    axios.post("http://localhost:3000/posts",{ todo })
+    .then(() => {
+      alert('Data is stored properly');
+    }).catch(() => {
+      alert('Data not stored');
+    })
+  }
+
+    function get(){
+      axios.get("http://localhost:3000/posts")
+      .then((res)=> {
+        setDb(res.data);
+      })
+      .catch((err)=>{
+        console.error(err);
+      })
+    }
+    console.log(db);  
+
+    useEffect(()=>{
+      get();
+    },[todo]);
+
+  function update(data,id){
+    axios.put(`http://localhost:3000/posts/${id}`,{ todo: data })
+    .then(() =>{
+      get();
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+  }
+
+  function deletedata(id){
+    axios.delete(`http://localhost:3000/posts/${id}`)
+    .then(()=>{
+      get();
+    })
+    .catch((err)=>{
+      console.error(err);
+    })
+  }
   return (
-    <div>Todo</div>
+    <div style={{margin: '100px'}}>
+          <label htmlFor="todo">Todo</label>
+          <input type="text" value={todo} onChange={(e)=> setTodo(e.target.value)}/>
+          <button onClick={posts}>POST</button>
+          {/* {todo}   */}
+          
+          <ul>
+              
+              {
+                
+                              db.map((data)=> (
+                               <li key = {data.id}>
+                                  {data.todo}
+                                <button onClick ={()=>{
+                                  const item = prompt("Enter the Data",data.todo).trim();
+                                  update(item,data.id)} 
+                                } >Update
+                                </button>
+                                <button onClick={()=>{
+                                  
+                                }}></button>
+                                </li>
+                              ))
+                }
+            </ul>
+          {/* <ul>
+            {
+              db.map((item)=>{
+                return <li key={item.id}>{item.todo}</li>
+              })
+            }
+          </ul> */}
+
+
+    </div>
+
+
   )
 }
 
-export default Todo
+export default Todo;
